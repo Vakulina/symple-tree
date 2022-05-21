fdd04640f171cddbaa8a906cb3c4fd9a45c43310
Vakulina <viktoria.ganina@yandex.ru>
2022-05-21 16:38:54 +0300

    fix:fix error of marking active item

--- src/component/ItemOfTree/ItemOfTree.js
+++ src/component/ItemOfTree/ItemOfTree.js
@@ -1,85 +1,84 @@
-import { useEffect, useState  } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import "./ItemOfTree.css"
 
-function ItemOfTree({ item, onClick, isDisplayChildren,  className }) {
+function ItemOfTree({ item, isDisplayChildren, activeItem, handleMarkingItem, className, onClick}) {
   const [childrenList, setChildrenList] = useState(null) //элементы списка
   const [visibilityChildren, setVisibilityChildren] = useState([]); //состояние показа подсписка для каждого элемента открытого списка
-  const [itemClasses, setItemClasses] = useState([])
-  const [outOfItem, setOutOfItem] = useState(false)
+ //console.log(activeItemPath)
+ const getActiveElementFromPath =(path)=>{
+   return path.slice(path.lastIndexOf("/")+1)
+ }
+
+//const [activeElement, setActiveElement] = useState(activeItem)
+//const [activeElement, setActiveElement] = useState( getActiveElementFromPath(activeItem)||null)
 
 
   const handleClick = (e, i, index) => { //свернуть/развернуть 
     e.stopPropagation();
-    console.log(visibilityChildren)
-    if (i['childNodes']) {
+    handleMarkingItem(i.id)
+ 
+    if (i['nodes']) {
       const newVisibilityChildren = [...visibilityChildren];
       newVisibilityChildren[index] = !visibilityChildren[index];
       setVisibilityChildren(newVisibilityChildren);
+      console.log("KKK")
+     
     }
     else {
-      const newClasses = [...itemClasses.map((el, elIndex) => { 
-        return el = (elIndex === index) ? 'active' : '';
-
-      })]
-     
-      setItemClasses(newClasses)
     }
   }
 
   useEffect(() => {
     const arr = [];
-    const classes = [];
-    const children = item["childNodes"];
-
+    const children = item["nodes"];
     if (item && children) {
       setChildrenList(children);
-
       children.forEach((i, index) => { //используем индекс массива,т.к. порядок элементов массива у нас не меняется и наше API не предоставляет уникальный id
         arr[index] = false;
-        classes[index] = ''
-      });
+         });
       setVisibilityChildren(arr);
-      setItemClasses(classes)
     }
-
+    
+   // setActiveElement(getActiveElementFromPath(activeItem))
   }, [item]);
-
-  useEffect(()=>{
-
-  },[])
 
   const getClassName = (index, i) => {
     if (visibilityChildren[index]) {
       return 'item_opened'
     }
-    else if (!visibilityChildren[index] && i["childNodes"]) {
+    else if (!visibilityChildren[index] && i["nodes"]) {
       return 'item_closed'
+    }
+    else {
+  const classStr = (activeItem === i.id) ? 'active' : '';
+     
+    return classStr
     }
   }
 
-  const renderDisplayChildren =()=>{ 
-    return (
-    <>{childrenList && isDisplayChildren &&
-      <ul>
-        {
-          childrenList.map((i, index) => (
-            <ItemOfTree
-              item={i}
-              key={index}
-              isDisplayChildren={visibilityChildren[index]}
-              onClick={(e) => handleClick(e, i, index)}
-              className={`${getClassName(index, i)} ${itemClasses[index]}`}
-              
-            />
-          ))
-        }
-      </ul>}
-    </>
-  );}
-
-
-
+  const renderDisplayChildren = () => {
+    if (childrenList && isDisplayChildren) {
+      return (
+        <ul>
+          {
+            childrenList.map((i, index) => (
+              <ItemOfTree
+                item={i}
+                key={i.id}
+    
+                isDisplayChildren={visibilityChildren[index]}
+                onClick={(e) => handleClick(e, i, index)}
+                className={`${getClassName(index, i)} }`}
+                handleMarkingItem={ handleMarkingItem}
+                activeItem={activeItem}
+              />
+            ))
+          }
+        </ul>
+      );
+    }
+  }
   return (
     <li onClick={onClick} className={className}  >
       {item && item["title"]}
--- src/component/Tree/Tree.js
+++ src/component/Tree/Tree.js
@@ -3,29 +3,37 @@ import React from "react";
 import ItemOfTree from '../ItemOfTree/ItemOfTree';
 import './Tree.css'
 
- function Tree() {
+
+function Tree() {
   const [list, setList] = useState({})
-  const items=require('../../test.json')
-
-
-  
+  const items = require('../../test1.json')
+  const [activeItem, setActiveItem] = useState('')
 
   useEffect(() => {
-   /*
-    fetchDataList()
-    .then((list)=>{
-     setList(list)})*/
-     setList(items)
-     console.log(items.childNodes)
-     
+    /*
+     fetchDataList()
+     .then((list)=>{
+      setList(list)})*/
+    setList(items)
   }, [])
 
-  
+useEffect(()=>{
+  console.log("new activeItemPath,", activeItem)
+},[activeItem])
+
+  const handleClick = (id) => {
+    setActiveItem(id)
+    console.log("TRREE")
+  }
+
   return (
     <>
-      <ItemOfTree item={list} isDisplayChildren={true}  />
-     
+      <ItemOfTree item={list}
+        isDisplayChildren={true}    
+        handleMarkingItem={handleClick}
+        activeItem={activeItem}
+      />
     </>
   )
 }
-export default React.memo(Tree)
+export default Tree
