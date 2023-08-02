81d3ff4caf7efdb86799e63fa1ef3d4140889431
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-08-02 11:16:00 +0300

    fix server's link

--- frontend/src/component/Tree/Tree.js
+++ frontend/src/component/Tree/Tree.js
@@ -8,27 +8,32 @@ import fetchDataList from "../../utiles/fetchDataList";
 function Tree() {
   const [list, setList] = useState({})
   const [activeItem, setActiveItem] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-     fetchDataList()
-     .then((list)=>{
-      setList(list)})
+    fetchDataList()
+      .then((list) => {
+        setList(list)
+        setIsLoading(false)
+      })
   }, [])
 
   const handleClick = (id) => {
     setActiveItem(id)
   }
 
-  return (
-    <ul className="tree">
-      <img src={ bellImg } alt="bell"/>
-      <span className="tree-name">master</span>
-      <ItemOfTree item={list}
-        isDisplayChildren={true}
-        handleMarkingItem={handleClick}
-        activeItem={activeItem}
-      />
-    </ul>
-  )
+  return (<>
+    {isLoading ? <span>isLoading...</span> :
+      <ul className="tree">
+        <img src={bellImg} alt="bell" />
+        <span className="tree-name">master</span>
+        <ItemOfTree item={list}
+          isDisplayChildren={true}
+          handleMarkingItem={handleClick}
+          activeItem={activeItem}
+        />
+      </ul>
+    }
+  </>)
 }
 export default Tree;
